fa10ad4031205b98a91d10c55a6a21b1396ee7c8
Eddy Varela <eddvar@amazon.com>
2024-01-14 00:21:15 -0800

    adding the ability to update customers

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -11,6 +11,20 @@ class CustomersController < ApplicationController
     @customer = Customer.new
   end
 
+  def edit 
+    @customer = Customer.find(params[:id])
+  end
+
+  def update
+    @customer = Customer.find(params[:id])
+
+    if @customer.update(customer_params)
+      redirect_to @customer
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+  
   def create
     @customer = Customer.new(customer_params)
 
